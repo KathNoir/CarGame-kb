28aa198ea3a3f2e9e4aee25bb426cdf834040bb3
Blanco <0328414@students.dadeschools.net>
2019-02-13 14:04:10 -0500

    started car game

---
+++ main.js
@@ -0,0 +1,34 @@
+// variables
+let car = {
+    make: 'Tesla',
+    model: 'S Refresh',
+    color: 'white',
+    milage: 0,
+    isWorking: true,
+
+    driveToWork(){
+        alert('Old Mileage: ${this.mileage}');
+        this.mileage += 10;
+        alert('New Mileage: ${this.mileage}');
+    }
+}
+
+// functions
+//logs current car stats to console
+/*
+function reWriteStats(){
+
+}
+*/
+
+//main process
+//keyboard inputs
+document.onkeyup = (event) => {
+    // captures key presses, makes lowercase and saves it as a variable
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (letter === "d") {
+        car.driveToWork();
+        reWriteStats();
+    }
+}
