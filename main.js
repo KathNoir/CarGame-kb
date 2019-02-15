a26a9be5ee97215d5b7351dfbb3d72037126dee9
Blanco <0328414@students.dadeschools.net>
2019-02-15 14:05:14 -0500

    finished game part

--- main.js
+++ main.js
@@ -9,25 +9,59 @@ let car = {
         alert( `Old Mileage: ${this.mileage}`);
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
+    },
+    driveAroundUSA(){
+        alert( `Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+    honk(){
+        alert('honk honk');
     }
 }
 
 // functions
 //logs current car stats to console
-/*
-function reWriteStats(){
-
+reWriteStats=()=>{
+    console.log(`Make: ${car.make}`)
+    console.log(`Model: ${car.model}`)
+    console.log(`Mileage: ${car.mileage}`)
+    console.log(`Color: ${car.color}`)
+    console.log(`isWorking: ${car.isWorking}`)
 }
-*/
+
 
 //main process
 //keyboard inputs
 document.onkeyup = (event) => {
     // captures key presses, makes lowercase and saves it as a variable
     let letter = String.fromCharCode(event.keyCode).toLowerCase();
-
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+if(car.isWorking===true){
+    
     if (letter === "d") {
         car.driveToWork();
         reWriteStats();
     }
+    if (letter === "h") {
+        car.honk();
+        reWriteStats();
+    }
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+}
+    else {
+    console.log(`your car need a tuneup!`);
+}
 }
